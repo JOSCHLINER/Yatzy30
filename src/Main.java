b143e2ac07a713533f7061be1ad24e089d5e30eb
Joschliner <56517095+JOSCHLINER@users.noreply.github.com>
2024-03-12 13:15:57 +0000

    added game start option

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String gameStartMessage = "Press y to play n to exit the game";
 
-        new Game();
+        System.out.println(gameStartMessage);
+        while (scanner.hasNext()) {
+
+
+            String item = scanner.next();
+            if (item.equals("y")) {
+                System.out.println("Starting Game...");
+                new Game();
+            } else if (item.equals("n")) {
+                System.out.println("Stopping Game");
+                break;
+            }
+
+            System.out.println(gameStartMessage);
+        }
+
     }
 
 }
